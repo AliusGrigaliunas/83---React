854209c3e2fa37182d22ff101422716cd9c31a14
AliusGrigaliunas <Alius.grigaliunas@gmail.com>
2022-08-30 18:42:12 +0300

    feat(83): finished reading reactjs docs, watched 36min of react crash course

---
+++ src/components/classBasedElement.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+class ClassElement extends React.Component {
+    render(): React.ReactNode {
+        return(
+            <h2>This is a class</h2>
+        )
+    }
+}
+
+export default ClassElement;
