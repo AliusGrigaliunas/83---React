529b678626e2931dd0cdb54b960fac66cbea09d0
AliusGrigaliunas <Alius.grigaliunas@gmail.com>
2022-08-30 20:37:02 +0300

    feat(83):finished 82nd HW, and tested map functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import ClassElement from './components/classBasedElement'
 
+
+
+
+type user = {
+  name: string,
+  surname:string,
+}
+
 function App() {
+  const users: user[] = [{
+    name: 'Alius',
+    surname: 'Grigaliūnas'
+  },{
+    name:'Lukas',
+    surname:'Ignatjevas',
+  }] 
+
+  const ReturnUser = users.map((user, i) => {
+    return <p key={i}>
+      {user.name}
+    </p>;
+  })
+
+  console.log(ReturnUser);
+
   return (
     <div className="App">
       <h1>This is a function component</h1>
       <ClassElement />
+      {ReturnUser}
     </div>
   );
 }
